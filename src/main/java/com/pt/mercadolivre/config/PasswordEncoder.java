72c601634fcbf1d2f7dd7655ca0b43cd65308c9a
faellarujo <raphaelaraujo.ti@gmail.com>
2024-06-14 16:38:45 +0100

    Add BCryptPasswordEncoder para criação do hash

---
+++ src/main/java/com/pt/mercadolivre/config/PasswordEncoder.java
@@ -0,0 +1,10 @@
+package com.pt.mercadolivre.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BCryptPasswordEncoder {
+
+
+}
---
+++ src/main/java/com/pt/mercadolivre/service/UserService.java
@@ -0,0 +1,2 @@
+package com.pt.mercadolivre.service;public class UserService {
+}
