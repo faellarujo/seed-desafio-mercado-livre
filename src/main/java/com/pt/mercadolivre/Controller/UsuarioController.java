e82f845f90c49945a300ff536d3c9849edd4fd7b
faellarujo <raphaelaraujo.ti@gmail.com>
2024-07-08 11:03:52 +0100

    Retorno do Token ao cadastrar usuario

--- src/main/java/com/pt/mercadolivre/Controller/UsuarioController.java
+++ src/main/java/com/pt/mercadolivre/Controller/UsuarioController.java
@@ -2,30 +2,31 @@ package com.pt.mercadolivre.Controller;
 
 import com.pt.mercadolivre.Request.UsuarioRequest;
 import com.pt.mercadolivre.repository.UsuarioRepository;
+import com.pt.mercadolivre.service.AuthenticationService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UsuarioController {
 
-
-
-
     @PersistenceContext
-    private EntityManager manager; //2
+    private EntityManager manager;
 
     @Autowired
-    private UsuarioRepository usuarioRepository; //3
-
+    private AuthenticationService authenticationService;
 
     @PostMapping("/usuario")
     @Transactional
-    public UsuarioRequest cadastrarUsuario(@RequestBody @Valid UsuarioRequest request){
+    public String cadastrarUsuario(@RequestBody @Valid UsuarioRequest request){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         manager.persist(request.toModel());
-        return request;
+        final String authenticate = authenticationService.authenticate(authentication);
+        return authenticate;
     }
 }
